ca503a8a013f60895d4f82c088eb261b8281cf42
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-09-12 16:00:33 +0100

    commit

--- src/component/Googlecloud.jsx
+++ src/component/Googlecloud.jsx
@@ -13,10 +13,14 @@ const MyComponent = () => {
     console.log('Place ID:', placeId);
 
     // Make a request to the Google Places Details API
-    https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${}&fields=geometry
+    // https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${}&fields=geometry
+
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const TARGET_URL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${'AIzaSyBRDlvJOTJYDmVsm3HKGeUjoZjgvlAxquE'}`
+const URL = PROXY_URL + TARGET_URL
 
     try {
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${'AIzaSyBRDlvJOTJYDmVsm3HKGeUjoZjgvlAxquE'}`,{
+      const response = await axios.get(URL,{
         headers: {
           'Content-Type': 'application/json',
           // You can't set Access-Control-Allow-Origin here; it's handled by the server
@@ -47,13 +51,40 @@ const MyComponent = () => {
           onChange: handleSelect,
         }}
         placeholder="Search for a location"
+        styles={{
+          container: {
+            flex: 1,
+            width: '100%',
+          },
+          textInputContainer: {
+            backgroundColor: 'transparent',
+            borderRadius: '0.375rem',
+            borderWidth: '1px',
+            borderColor: '#d1d5db',
+            width: '100%',
+            maxWidth: '100%',
+            margin: '0 auto',
+          },
+          textInput: {
+            height: '2.75rem',
+            borderColor: '#d1d5db',
+            borderWidth: '1px',
+            borderRadius: '0.375rem',
+            paddingHorizontal: '1rem',
+            width: '100%',
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+        }}
       />
-      {location && (
+      
+      {/* {location && (
         <div>
           <p>Latitude: {location.lat}</p>
           <p>Longitude: {location.lng}</p>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
