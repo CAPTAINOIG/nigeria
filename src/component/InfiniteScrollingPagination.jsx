1e156f92b11aa87bacc2ce42c875651adc520d11
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2024-12-19 16:47:04 +0100

    commit

--- src/component/InfiniteScrollingPagination.jsx
+++ src/component/InfiniteScrollingPagination.jsx
@@ -16,7 +16,7 @@ const InfiniteScrollPagination = () => {
             if (newItems.length > 0) {
                 setItems((prevItems) => [...prevItems, ...newItems]);
             } else {
-                setHasMore(false); 
+                setHasMore(false);
             }
         } catch (error) {
             console.error("Error fetching data:", error);
@@ -37,31 +37,31 @@ const InfiniteScrollPagination = () => {
 
     return (
         <div className="bg-gray-200 h-screen">
-            <div className="grid grid-cols-1 bg-gray-200 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-4 p-10">
-                {items.map((item, index) =>{ 
+            <div className="grid grid-cols-1 bg-gray-200 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 p-10">
+                {items.map((item, index) => {
                     return (
-                    <div key={index} className="rounded-lg">
-                        <div className="text-black font-bold text-center mb-2 text-xs">Item {item.Id}</div>
-                        <div className="flex justify-center items-center relative">
-                        <div className="absolute mt-[120px] text-orange-500 text-[8px] transparent rounded-full p-1 lg:ms-[80px] font-bold z-50">Item {item.Id}</div>
+                        <div key={index} className="rounded-lg py-2 px-1 relative">
+                            <div className="text-black font-bold text-center mb-2 text-xs">Item {item.Id}</div>
+                            <div className="absolute bottom-[3%] right-[8%] text-gray-500 text-[9px] transparent rounded-full p-1 lg:ms-[80px] font-extrabold z-50">Item {item.Id}</div>
                             <img className="w-full h-auto" src={item?.MemQrCode} alt="QR Code" />
+
                         </div>
-                    </div>
-                )})}
+                    )
+                })}
             </div>
 
-            {loading  &&(
+            {loading && (
                 <div className="flex justify-center items-center bg-gray-200">
                     <img src={gif} alt="Loading..." className="w-[30px] mt-10" />
                 </div>
-            ) }
+            )}
             {hasMore && !loading && (
                 <div className="flex justify-center bg-gray-200">
                     <button onClick={handleLoadMore} className="bg-blue-900 mb-3 hover:bg-gray-900 text-white px-4 py-2 rounded">
                         Load More
                     </button>
                 </div>
-            ) } {!hasMore && (
+            )} {!hasMore && (
                 <p className="text-center text-gray-700 mt-5">No more items to load</p>
             )}
         </div>
