55f0416db6f64efe22e8e208745e219eb3f9df64
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-01-08 16:33:17 +0100

    new changes made

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -9,8 +9,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://lendnode.creditclan.com/memo/api',
-    baseURL: 'https://4478-2a09-bac5-4dd3-6d2-00-ae-1b.ngrok-free.app/api',
+    baseURL: 'https://lendnode.creditclan.com/memo/api',
+    // baseURL: 'https://4478-2a09-bac5-4dd3-6d2-00-ae-1b.ngrok-free.app/api',
     headers: {
       "ngrok-skip-browser-warning": '69420',
       'Content-Type': 'application/json',
