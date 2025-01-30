def5ac6456ae447023741860c686fe7e496d15bd
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-01-30 13:59:19 +0100

    commit

--- src/component/InfiniteScrollingPagination.jsx
+++ src/component/InfiniteScrollingPagination.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import gif from '../assets/image/gif.gif';
 import axiosInstance from "../axiosInstance";
-import {toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { useSearchParams } from 'react-router-dom';
 
 const InfiniteScrollPagination = () => {
@@ -18,6 +18,7 @@ const InfiniteScrollPagination = () => {
         try {
             const response = await axiosInstance.get(`/memo/get_all_memoqrcodes?page=${page}`);
             const newItems = response?.data?.data || [];
+            console.log(newItems);
             // if (newItems.length > 0) {
             //     setItems((prevItems) => [ ...newItems]);
             // }
@@ -29,8 +30,8 @@ const InfiniteScrollPagination = () => {
             console.error("Error fetching data:", error);
             toast.error(`Error fetching data: ${error.message}`);
             setError(`Error fetching data: ${error.message}`);
-        }finally{   
-            setLoading(false);  
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -46,14 +47,48 @@ const InfiniteScrollPagination = () => {
 
     return (
         <div className="bg-gray-200 h-screen">
-            <div className="grid grid-cols-1 bg-gray-200 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 p-10">
+            <div className="grid grid-cols-1 bg-gray-200 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-10 gap-10">
                 {items.length > 0 && items.map((item, index) => {
                     return (
                         <div key={index} className="rounded-lg py-2 px-1 relative">
-                            <div className="text-black font-bold text-center mb-2 text-xs">Item {item.Id}</div>
-                            <div className="absolute bottom-[2%] right-[8%] text-gray-500 text-[9px] transparent rounded-full p-1 lg:ms-[80px] font-extrabold z-50">Item {item.Id}</div>
-                            <img className="w-full h-auto" src={item?.MemQrCode} alt="QR Code" />
+                            {/* <div className="text-black font-bold text-center mb-2 text-xs">Item {item.Id}</div> */}
+                            {/* <div className="absolute bottom-[2%] right-[8%] text-gray-500 text-[9px] transparent rounded-full p-1 lg:ms-[80px] font-extrabold z-50">Item {item.Id}</div> */}
 
+                            <div className="absolute bottom-[2%] right-[15%] lg:right-[8%] text-gray-500 text-[9px] bg-transparent rounded-full p-1 font-extrabold z-50 max-w-full">
+                                Item {item.Id}
+                            </div>
+
+                            {/* <div className=" p-1 bg-[#002f6c] rounded-lg text-center relative">
+                                <div className="h-[5px] bg-[#f26d22] rounded-t-lg"></div>
+                                <div className="rounded-md">
+                                    <img className="object-contain" src={item?.MemQrCode} alt="QR Code" />
+                                </div>
+                                <span className="text-white text-[10px] font-bold block mt-1">SCAN ME</span>
+                            </div> */}
+
+
+                            {/* <div className="qr-container p-1 bg-[#002f6c] rounded-lg text-center relative">
+                                <div className="h-[50%] bg-[#33f222] w-full absolute top-0 left-0 rounded-t-lg"></div>
+
+                                <div className="relative p-1 flex items-center justify-center">
+                                    <img className="object-contain relative z-10" src={item?.MemQrCode} alt="QR" />
+                                </div>
+
+                                <span className="text-white text-1xl font-bold block mt-1 relative z-10 py-4">SCAN ME</span>
+                            </div> */}
+                            <div className="qr-container w-[270.64px] h-[313.46px] bg-[#002f6c] rounded-lg text-center relative flex flex-col items-center gap-5 p-4">
+                                <div className="absolute top-0 left-0 w-full h-1/2 bg-[#33f222] rounded-t-lg"></div>
+                                <div className="relative flex items-center justify-center z-10 gap-5">
+                                    <img
+                                        className="object-contain w-[236.22px] h-[236.22px]"
+                                        src={item?.MemQrCode}
+                                        alt="QR Code"
+                                    />
+                                </div>
+                                <span className="text-white text-lg font-bold relative z-10 py-2">
+                                    SCAN ME
+                                </span>
+                            </div>
                         </div>
                     )
                 })}
@@ -70,7 +105,7 @@ const InfiniteScrollPagination = () => {
                     <p className="text-red-500">{error}</p>
                 </div>
             )}
-        
+
         </div>
     );
 };
